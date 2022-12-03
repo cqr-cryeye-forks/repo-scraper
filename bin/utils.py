8a7d12188bff900a15893311c4da9af22b15b65d
zeinlol <nickolaiborshchov@gmail.com>
2022-12-03 11:36:54 +0000

    updating saving results

--- bin/utils.py
+++ bin/utils.py
@@ -1,11 +1,11 @@
 from bin.arguments import args
-from repo_scraper.constants.result import ALERT, WARNING, NOTHING
+from repo_scraper.constants.result import ALERT, WARNING, NOTHING, ERROR
 
 
 def get_results_level() -> list:
-    results_to_print = [ALERT]
+    results_to_print = [ALERT, ERROR]
     if args.warnings:
-        results_to_print += [WARNING]
+        results_to_print.append(WARNING)
     elif args.printall:
-        results_to_print += [WARNING, NOTHING]
+        results_to_print.extend([WARNING, NOTHING])
     return results_to_print
