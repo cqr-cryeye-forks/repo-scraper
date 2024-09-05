922bc1941deb030ba7908092e0a159f2771ee3c4
Evil2997 <dima.molchanow2012@gmail.com>
2024-09-05 12:01:41 +0300

    Worked with all types, but have extra conclusion

--- repo_scraper/FileChecker.py
+++ repo_scraper/FileChecker.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 from bin.arguments import args
@@ -6,29 +5,11 @@ from repo_scraper import matchers as m, filetype
 from repo_scraper.Result import *
 from repo_scraper.constants.git_diff import MAX_DIFF_ADDITIONS_CHARACTERS
 
-
 class FileChecker:
     def __init__(self, path, allowed_extensions, max_file_size_bytes=MAX_DIFF_ADDITIONS_CHARACTERS):
         self.path = path
         self.max_file_size_bytes = max_file_size_bytes
         self.allowed_extensions = allowed_extensions
-
-    def check_json_for_secrets(self, json_content):
-        matches = []
-        for key, value in json_content.items():
-            if isinstance(value, str):
-                # Применяем регулярные выражения для проверки значений
-                match, _ = m.multi_matcher(value.strip(),
-                                           m.password_matcher,
-                                           m.git_token_matcher,
-                                           m.ssh_key_matcher,
-                                           m.strong_password_matcher)
-                if match:
-                    matches.append([None, f"{key}: {value.strip()}"])  # Номера строки нет, так как это JSON
-            elif isinstance(value, dict):
-                # Рекурсивно проверяем вложенные словари
-                matches.extend(self.check_json_for_secrets(value))
-        return matches
 
     def check(self):
         comments = []
@@ -43,23 +24,19 @@ class FileChecker:
             return Result(self.path, FILETYPE_NOT_ALLOWED)
 
         matches = []
-        if self.path.endswith('.json'):
-            with open(self.path, 'r', encoding='utf-8') as f:
-                try:
-                    json_content = json.load(f)
-                    matches.extend(self.check_json_for_secrets(json_content))
-                except json.JSONDecodeError:
-                    pass  # Пропускаем файлы с ошибками в JSON
-        else:
-            with open(self.path, 'r', encoding='utf-8') as f:
-                for i, line in enumerate(f, 1):
+        with open(self.path, 'r', encoding='utf8') as f:
+            try:
+                for i, line in enumerate(f, 1):  # Чтение файла построчно с сохранением номера строки
                     match, _ = m.multi_matcher(line.strip(),
                                                m.password_matcher,
+                                               m.ip_matcher,
                                                m.git_token_matcher,
                                                m.ssh_key_matcher,
                                                m.strong_password_matcher)
                     if match:
-                        matches.append([i, line.strip()])
+                        matches.append([i, line.strip()])  # Сохраняем номер строки и строку
+            except UnicodeDecodeError:
+                return Result(self.path, FILETYPE_NOT_ALLOWED)
 
         if matches:
             return Result(self.path, MATCH, matches=matches, comments=comments)
